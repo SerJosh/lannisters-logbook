f65857f48500dcdb37b09cf63392b74240c0175d
SerJosh <>
2023-10-12 21:37:17 +0000

    this is now just an experemental page and will eventually be deleted

--- run.py
+++ run.py
@@ -1,4 +1,30 @@
-from prettytable import PrettyTable, ALL
-from colorama import Fore, Back, Style
+#Building an experimental Budgeting tool
+#income - expenses = margin
 
-print("hello world not, installed beautiful table")
+
+print("Welcome to the Budget Estimator \n")
+income = float(input("Enter your monthly post-tax income: "))
+additional = float(input("Enter any additional income you expect this month: "))
+
+total_income = income + additional 
+print("your total income this month will be: " + str(total_income)+ "\n")
+print("Now lets get some expenses...soz \n")
+
+def gather_expenses():
+    housing = float(input("Enter your monthly housing expense: "))
+    utilities = float(input("Enter your monthly utilities total expense: "))
+    food = float(input("Enter your monthly food expense: "))
+    misc = float(input("Enter any additional expense: "))
+    total_exepenses = housing + utilities + food + misc
+    return total_exepenses
+
+expense_total = gather_expenses()
+print("your total expenses this month will be: " + str(expense_total)+ "\n")
+margin = total_income - expense_total
+if margin >= 0:
+    print("Your total surplus this coming month will be: " + str(margin) + "\n")
+else:
+    print("You will come up " + str(margin) + " short \n")
+
+print("thanks for using the monthly budget tool")
+
